52917f85eac9a9bac21d9f62f5fa4ad49bfe867c
Rimi999 <all_wom@naver.com>
2021-06-09 13:15:31 +0900

    stage

--- js/index.js
+++ js/index.js
@@ -1,11 +1,30 @@
-/*************** 글로벌 설정 *****************/
+$(function() {
+	
+	/*************** 글로벌 설정 *****************/
+	var time;
+	var timeDivison;
+	var $stageWrapper = $('.stage-wrapper');
 
 
-/*************** 사용자 함수 *****************/
+	/*************** 사용자 함수 *****************/
+	init();
+	function init(){
+		var d = new Date();
+		time = d.getHours();
+		timeDivison =
+		(time >= 02	&& time < 06) ? 1:
+		(time >= 06	&& time < 10) ? 2:
+		(time >= 10 && time < 14) ? 3:
+		(time >= 14 && time < 18) ? 4:
+		(time >= 18 && time < 22) ?	5:6;
+
+		for(var i=1; i<=6; i++) $stageWrapper.removeClass('active'+i);
+		$stageWrapper.addClass('active'+timeDivison);
+	}
 
 
-/*************** 이벤트 등록 *****************/
+	/*************** 이벤트 등록 *****************/
 
 
-/*************** 이벤트 콜백 *****************/
-
+	/*************** 이벤트 콜백 *****************/
+});
