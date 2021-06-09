d8bed6bafc51c75c94849eb5bc887134e4dd6f90
Rimi999 <all_wom@naver.com>
2021-06-09 13:55:47 +0900

    bg-wrapper

--- js/index.js
+++ js/index.js
@@ -3,12 +3,24 @@ $(function() {
 	/*************** 글로벌 설정 *****************/
 	var time;
 	var timeDivison;
-	var $stageWrapper = $('.stage-wrapper');
+	var weatherIcon = {
+		i01: 'bi-brightness-high',
+		i02: 'bi-cloud-sun',
+		i03: 'bi-cloud',
+		i04: 'bi-clouds',
+		i09: 'bi-cloud-rain-heavy',
+		i10: 'bi-cloud-drizzle',
+		i11: 'bi-cloud-lightning',
+		i13: 'bi-cloud-snow',
+		i50: 'bi-cloud-haze',
+	}
+	var $bgWrapper = $('.bg-wrapper');
 
 
 	/*************** 사용자 함수 *****************/
-	init();
-	function init(){
+	initBg ();
+
+	function initBg() {
 		var d = new Date();
 		time = d.getHours();
 		timeDivison =
@@ -17,12 +29,13 @@ $(function() {
 		(time >= 10 && time < 14) ? 3:
 		(time >= 14 && time < 18) ? 4:
 		(time >= 18 && time < 22) ?	5:6;
-
-		for(var i=1; i<=6; i++) $stageWrapper.removeClass('active'+i);
-		$stageWrapper.addClass('active'+timeDivison);
+		for(var i=1; i<=6; i++) $bgWrapper.removeClass('active'+i);
+		$bgWrapper.addClass('active'+timeDivison);
 	}
-
 	
+
+
+
 	/*************** 이벤트 등록 *****************/
 
 
